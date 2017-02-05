27897d6fcbea441026f3d75670d895d7f988f2f9
Jaroslaw Pawlak <jaroslaw.pawlak@gmail.com>
2017-02-05 16:41:57 +0000

    robot w miarę działający ale nie przez flask

--- Programy/Raspbian/Robot/robot.py
+++ Programy/Raspbian/Robot/robot.py
@@ -1,42 +1,108 @@
 #obsuga robota
 import time 
 import socket 
-
+#import pygame
+import RPi.GPIO as GPIO
+AIN1 = 17 #lewy przod
+AIN2 = 27 #lewy tyl
+BIN1 = 22 #prawy tyl
+BIN2 = 23 #prawy przod
+Frequency = 50
 
 class Robot:
     def __init__(self):
-        print('initialize')
-        return
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(AIN1, GPIO.OUT)
+        GPIO.setup(AIN2, GPIO.OUT) 
+        GPIO.setup(BIN1, GPIO.OUT)
+        GPIO.setup(BIN2, GPIO.OUT)
+        self.motorLeftForward = GPIO.PWM(AIN1, Frequency)
+        self.motorLeftBack = GPIO.PWM(AIN2, Frequency)
+        self.motorRightForward = GPIO.PWM(BIN2, Frequency)
+        self.motorRightBack = GPIO.PWM(BIN1, Frequency)
+        self.motorLeftForward.start(0)
+        self.motorLeftBack.start(0)
+        self.motorRightForward.start(0)
+        self.motorRightBack.start(0)
+
+    def __del__(self):
+        GPIO.cleanup()
+
 
     def goForward(self):
-        print('going forward')       
+        print('going forward')
+        (GPIO.output(AIN1, GPIO.HIGH))
+        (GPIO.output(AIN2, GPIO.LOW))
+        (GPIO.output(BIN1, GPIO.LOW))
+        (GPIO.output(BIN2, GPIO.HIGH))
         return
 
     def goBack(self):
-        print('going back')       
+        print('going back')
+        (GPIO.output(AIN1, GPIO.LOW))
+        (GPIO.output(AIN2, GPIO.HIGH))
+        (GPIO.output(BIN1, GPIO.HIGH))
+        (GPIO.output(BIN2, GPIO.LOW))
         return
 
     def turnRight(self):
         print('turn right')
+        (GPIO.output(AIN1, GPIO.HIGH))
+        (GPIO.output(AIN2, GPIO.LOW))
+        (GPIO.output(BIN1, GPIO.HIGH))
+        (GPIO.output(BIN2, GPIO.LOW))
         return
 
     def turnLeft(self):
         print('turn left')
-
+        (GPIO.output(AIN1, GPIO.LOW))
+        (GPIO.output(AIN2, GPIO.HIGH))
+        (GPIO.output(BIN1, GPIO.LOW))
+        (GPIO.output(BIN2, GPIO.HIGH))
         return
     
     def stop(self):
-        print('stop')
+        (GPIO.output(AIN1, GPIO.LOW))
+        (GPIO.output(AIN2, GPIO.LOW))
+        (GPIO.output(BIN1, GPIO.LOW))
+        (GPIO.output(BIN2, GPIO.LOW))
         return
-
     def move(self, speed, direction):
-        speed = speed * 4
-        speed = 100 if speed > 100.0 else speed
-        speed = -100 if speed < -100.0 else speed
-        direction = direction * 4
-        direction = 100 if direction > 100.0 else direction
-        direction = -100 if direction < -100.0 else direction
-        print('going with', speed, 'speed and direction: ', direction)
-        return 
+        print('move started1')
+        print('1')
+        s = int(speed) * 4
+        s = 100 if s > 100 else s
+        s = -100 if s < -100 else s
+        d = int(direction) * 4
+        d = 100 if d > 100 else d
+        d= -100 if d < -100 else d
+#        print('going with', s, 'speed and direction: ', d)
+        print('2')
+        leftEngineSpeed = (s + d) /2
+        rightEngineSpeed = (s - d) /2
+
+        if leftEngineSpeed > 0 :
+            self.motorLeftBack.ChangeDutyCycle(0)
+            self.motorLeftForward.ChangeDutyCycle(leftEngineSpeed)
+        elif leftEngineSpeed < 0:
+            self.motorLeftForward.ChangeDutyCycle(0)
+            self.motorLeftBack.ChangeDutyCycle(-leftEngineSpeed)
+        else:
+            self.motorLeftForward.ChangeDutyCycle(0)
+            self.motorLeftBack.ChangeDutyCycle(0)
+
+        if rightEngineSpeed > 0 :
+            self.motorRightBack.ChangeDutyCycle(0)
+            self.motorRightForward.ChangeDutyCycle(rightEngineSpeed)
+        elif rightEngineSpeed <0:
+            self.motorRightForward.ChangeDutyCycle(0)
+            self.motorRightBack.ChangeDutyCycle(-rightEngineSpeed)
+        else:
+            self.motorRightForward.ChangeDutyCycle(0)
+            self.motorRightBack.ChangeDutyCycle(0)
+
+        
+        return "ok"
+
     def makePhoto(self):
         return
